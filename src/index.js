9a625499773fdddb3d3888cf04d262c53343cc3e
tracycollins <tc@threeceemedia.com>
2020-09-15 20:13:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 // ./src/index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux"; /* code change */
+import { Provider } from "react-redux"; /* code change */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer.js";
+import App from "./App";
+import "./index.css";
+
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> /* code change */,
+  document.getElementById("root")
+);
